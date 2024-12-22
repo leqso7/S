2e5c170b3de4590b989bc29c836071884b879266
leqso7 <leqso7@users.noreply.github.com>
2024-12-22 21:58:48 +0000

    deploy: c8e0f5263228f5dfd9587375783849e053a151c4

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/S/sw.js', { scope: '/S/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
